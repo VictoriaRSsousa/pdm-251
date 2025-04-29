75a1660725c1fd98dc9819fc1eddbad6aa4d3be2
VictoriaRSsousa <82991480+VictoriaRSsousa@users.noreply.github.com>
2025-04-29 19:52:59 -0300

    Create atividade02.dart

---
+++ Atividades/Atividade02/atividade02.dart
@@ -0,0 +1,76 @@
+// Agregação e Composição
+import 'dart:convert';
+
+class Dependente {
+  late String _nome;
+
+  Dependente(String nome) {
+    this._nome = nome;
+  }
+}
+
+class Funcionario {
+  late String _nome;
+  late List<Dependente> _dependentes;
+
+  Funcionario(String nome, List<Dependente> dependentes) {
+    this._nome = nome;
+    this._dependentes = dependentes;
+  }
+}
+
+class EquipeProjeto {
+  late String _nomeProjeto;
+  late List<Funcionario> _funcionarios;
+
+  EquipeProjeto(String nomeprojeto, List<Funcionario> funcionarios) {
+    _nomeProjeto = nomeprojeto;
+    _funcionarios = funcionarios;
+  }
+}
+
+void main() {
+  //CRIAÇÃO DE DEPENDENTES
+  var dependente1 = new Dependente('dependente1');
+  var dependente2 = new Dependente('dependente2');
+  var dependente3 = new Dependente('dependente3');
+  var dependente4 = new Dependente('dependente4');
+  var dependente5 = new Dependente('dependente5');
+  var dependente6 = new Dependente('dependente6');
+  var dependente7 = new Dependente('dependente7');
+  var dependente8 = new Dependente('dependente8');
+  var dependente9 = new Dependente('dependente9');
+  var dependente10 = new Dependente('dependente10');
+
+  //CRIAÇÃO DE FUNCIONARIOS
+  var funcionario1 = new Funcionario('funcionario1', [
+    dependente10,
+    dependente9,
+  ]);
+
+  var funcionario2 = new Funcionario('funcionario', [dependente8, dependente7]);
+
+  var funcionario3 = new Funcionario('funcionario', [dependente6, dependente5]);
+
+  var funcionario4 = new Funcionario('funcionario', [dependente4, dependente3]);
+
+  var funcionario5 = new Funcionario('funcionario', [dependente2, dependente1]);
+
+  //CRIAÇÃO DE EQUIPES
+
+  var equipe1 = new EquipeProjeto('nomeprojeto1', [funcionario5, funcionario4]);
+
+  var equipe2 = new EquipeProjeto('nomeprojeto1', [funcionario2, funcionario3]);
+
+  var equipe3 = new EquipeProjeto('nomeprojeto1', [funcionario1]);
+
+  // 1. Criar varios objetos Dependentes
+  // 2. Criar varios objetos Funcionario
+  // 3. Associar os Dependentes criados aos respectivos
+  //    funcionarios
+  // 4. Criar uma lista de Funcionarios
+  // 5. criar um objeto Equipe Projeto chamando o metodo
+  //    contrutor que da nome ao projeto e insere uma
+  //    coleção de funcionario
+  // 6. Printar no formato JSON o objeto Equipe Projeto.
+}
